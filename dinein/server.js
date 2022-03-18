4967f7665ed728dc906dc34b969f979b23ff693c
dedirizfigueroa <dediriz.figueroa@upr.edu>
2022-03-18 00:44:35 -0400

    Created home, info and sign up pages

--- dinein/server.js
+++ dinein/server.js
@@ -7,8 +7,22 @@ var app = express();
 app.set('view engine', 'ejs');
 app.use(express.static('public')); // Render static files 
                                    // (static files belong in the public dir)
+app.use('/images',express.static('public/images'))
+app.use('/css',express.static('public/css'))
 
 // Port to listen to
 app.listen(8080);
 // Routes:
 
+app.get('',(req,res) =>{
+    res.render('pages/index')
+})
+app.get('/signup.ejs',(req,res) =>{
+    res.render('pages/signup')
+})
+app.get('/info.ejs',(req,res) =>{
+    res.render('pages/info')
+})
+app.get('/index.ejs',(req,res) =>{
+    res.render('pages/index')
+})
