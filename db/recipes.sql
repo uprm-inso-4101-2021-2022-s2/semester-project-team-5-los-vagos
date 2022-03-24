5ce7558fda4603a35ef2397e858c33d8963a9229
James Rodriguez <jamesorodz@gmail.com>
2022-03-23 23:02:18 -0400

    changes to the schemas

--- db/recipes.sql
+++ db/recipes.sql
@@ -1,7 +1,7 @@
 CREATE TABLE recipes(
     recipe_id SERIAL PRIMARY KEY,
-    ingredients, -- comma separated list of ingredients?
-    rating, -- 0-10 float rating from other users
+    ingredients TEXT ARRAY, -- comma separated list of ingredients?
+    rating DECIMAL, -- 0-10 float rating from other users
     creator_id, -- the id referrence to the creator
-    steps -- comma separated list of the steps?
+    steps TEXT-- comma separated list of the steps?
      );
