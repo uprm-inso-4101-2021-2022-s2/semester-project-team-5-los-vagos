21b9dc39f927474b8cb3b04ab7343f0f779575be
James Rodriguez <jamesorodz@gmail.com>
2022-03-23 23:05:03 -0400

    changes to schemas

--- db/users.sql
+++ db/users.sql
@@ -1,7 +1,7 @@
 CREATE TABLE users(
     usr_id SERIAL PRIMARY KEY, 
-    usr_name TEXT, -- text that is the user's username
-    usr_password TEXT, -- text that is the user's password
+    usr_name TEXT NOT NULL, -- text that is the user's username
+    usr_password TEXT NOT NULL, -- text that is the user's password
     saved_recipes INTEGER ARRAY-- this will be a referrence to a 
                   --foreign key recipe id, maybe comma separated list?
 );
