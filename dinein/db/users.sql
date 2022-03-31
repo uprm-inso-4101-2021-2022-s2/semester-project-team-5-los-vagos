b7e334398f687b61260029c3ef353b7c40e65312
James Rodriguez <jamesorodz@gmail.com>
2022-03-31 13:58:14 -0400

    moved schemas

---
+++ dinein/db/users.sql
@@ -0,0 +1,8 @@
+CREATE TABLE users(
+    usr_id SERIAL PRIMARY KEY, 
+    usr_name TEXT UNIQUE NOT NULL, -- text that is the user's username
+    usr_password TEXT NOT NULL, -- text that is the user's password
+    saved_recipes INTEGER []-- this will be a referrence to a 
+                  --foreign key recipe id, maybe comma separated list?
+    
+);
