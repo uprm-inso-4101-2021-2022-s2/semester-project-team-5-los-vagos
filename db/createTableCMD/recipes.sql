4247706cc65ce154f7ed83ee0b093308cd3aa392
James Rodriguez <jamesorodz@gmail.com>
2022-03-31 13:16:01 -0400

    db scema

--- db/createTableCMD/recipes.sql
+++ db/createTableCMD/recipes.sql
@@ -1,7 +1,7 @@
 CREATE TABLE recipes(
     recipe_id SERIAL PRIMARY KEY,
     recipe_name UNIQUE NOT NULL,
-    ingredients TEXT ARRAY, -- comma separated list of ingredients?
+    ingredients TEXT [], -- comma separated list of ingredients?
     rating DECIMAL, -- 0-10 float rating from other users
     creator_id INTEGER, -- the id referrence to the creator
     steps TEXT,-- comma separated list of the steps?
