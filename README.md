6f650f5d68aed79671cb9eb2b552d3c52f49eabb
James Rodriguez <jamesorodz@gmail.com>
2022-04-03 15:37:16 -0400

    .

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 #### Notes
 -------------------------------
 In order to see changes locally, restart the server. Otherwise, install nodemon.
+The credentials may change for the db connections.
+15:51
+https://www.youtube.com/watch?v=_Mun4eOOf2Q
--- db/createTableCMD/recipes.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE recipes(
-    recipe_id SERIAL PRIMARY KEY,
-    recipe_name UNIQUE NOT NULL,
-    ingredients TEXT [], -- comma separated list of ingredients?
-    rating DECIMAL, -- 0-10 float rating from other users
-    creator_id INTEGER, -- the id referrence to the creator
-    steps TEXT,-- comma separated list of the steps?
-    FOREIGN KEY (creator_id) REFERRENCES users (usr_id)
-     );
